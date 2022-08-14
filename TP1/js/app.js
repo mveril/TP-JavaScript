579611f7bdbfad7ed2c733282320850e09a91ee5
Mickaël Véril <mika.veril@wanadoo.fr>
2022-08-14 12:42:36 +0200

    Remove line of code used for test

--- TP1/js/app.js
+++ TP1/js/app.js
@@ -1,7 +1,6 @@
 import { Counter } from "./Counter.js";
 
 const counter = new Counter(document.querySelector(".counter"));
-window.counter=counter;
 const btnDecrease = document.querySelector("#btn-decrease");
 btnDecrease.addEventListener("click",()=>counter.decrease());
 const btnReset = document.querySelector("#btn-reset");
